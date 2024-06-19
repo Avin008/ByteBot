58fbb513c2503570fa338a7a5aed5ee50ca62e26
avinash <avinashnanda008@gmail.com>
2024-06-19 23:24:56 +0530

    feat: quiz-bot

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Hono } from 'hono';
+import { sendWhatsAppMessage } from './whatsappService';
 
 type Env = {
 	AI: Ai;
+	ACCOUNT_SID: string;
+	AUTH_TOKEN: string;
+	RECIEVER_NUMBER: string;
+	SENDER_NUMBER: string;
 };
 
 const app = new Hono<{ Bindings: Env }>();
@@ -9,15 +14,19 @@ const app = new Hono<{ Bindings: Env }>();
 app.get('/', async (c) => {
 	const ai = c.env.AI;
 
-	const searchQuery = c.req.query('search') as string;
-
 	const messages = [
-		{ role: 'system', content: 'you are a coding assistant' },
-		{ role: 'user', content: searchQuery },
+		{
+			role: 'system',
+			content:
+				'Present primary to secondary school level science topics one at a time by posing questions and offering four multiple-choice options for the user to select the correct answer, ensuring not to reveal yourself as an AI or interact beyond presenting the options. start with the question and end with the options no greetings or any other interaction',
+		},
 	];
 
-	const response = await ai.run('@cf/mistral/mistral-7b-instruct-v0.1', { messages });
-	return c.json({ data: response });
+	const res: any = await ai.run('@cf/meta/llama-3-8b-instruct', { messages });
+
+	await sendWhatsAppMessage(c.env.ACCOUNT_SID, c.env.AUTH_TOKEN, c.env.RECIEVER_NUMBER, c.env.SENDER_NUMBER, res.response);
+
+	return c.json({ data: res });
 });
 
 export default app;
---
+++ src/whatsappService.ts
@@ -0,0 +1,25 @@
+async function sendWhatsAppMessage(accountSid: string, authToken: string, toNumber: string, fromNumber: string, messageBody: string) {
+	const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+
+	const data = new URLSearchParams();
+	data.append('To', `whatsapp:${toNumber}`);
+	data.append('From', `whatsapp:${fromNumber}`);
+	data.append('Body', messageBody);
+
+	try {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				Authorization: 'Basic ' + btoa(`${accountSid}:${authToken}`),
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: data,
+		});
+		const resp = await res.json();
+		return resp;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export { sendWhatsAppMessage };
